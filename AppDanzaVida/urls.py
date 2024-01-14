eb8b9ac88b28aa56094e1a57a5023e2e06f8d6a4
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2024-01-14 11:56:18 -0300

    alumnos OK

--- AppDanzaVida/urls.py
+++ AppDanzaVida/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/home')),
     path('home/', HomeView.as_view(), name='home'),
     path('alumno_nuevo', login_required(AlumnoCreateView.as_view()), name='alumno_create'),
+    path('alumno_list', login_required(AlumnoListView.as_view()), name='alumno_list'),
+    path('alumno_update/<int:pk>', login_required(AlumnoUpdateView.as_view()), name='alumno_update'),
+    path('alumno_delete/<int:pk>', login_required(AlumnoDeleteView.as_view()), name='alumno_delete'),
 ]
--- AppDanzaVida/views.py
+++ AppDanzaVida/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .forms import AlumnoForm
 from .models import Alumno
 
@@ -11,8 +12,30 @@ class HomeView(View):
     def get(self, request):
         return render(request, 'home.html')
     
-class AlumnoCreateView(CreateView):
+class AlumnoCreateView(CreateView, PermissionRequiredMixin, ListView):
     model = Alumno
     form_class = AlumnoForm
     template_name = 'alumno/alumno_create.html'
     success_url = reverse_lazy('alumno_list')
+    permission_required = 'AppDanzaVida.add_alumno'
+
+class AlumnoListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = Alumno
+    template_name = "alumno/alumno_list.html"
+    context_object_name = 'alumnos'
+    permission_required = 'AppDanzaVida.view_alumno'
+
+class AlumnoUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = Alumno
+    form_class = AlumnoForm
+    template_name = 'alumno/alumno_update.html'
+    success_url = reverse_lazy('alumno_list')
+    permission_required = 'AppDanzaVida.change_alumno'
+
+class AlumnoDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    model = Alumno
+    success_url = reverse_lazy('alumno_list')
+    permission_required = 'AppDanzaVida.delete_alumno'
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
