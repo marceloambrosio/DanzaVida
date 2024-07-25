d12a92fbda5b32cea5e248e7aadf810a561ebb03
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2024-07-25 20:17:13 -0300

    fix url

--- DanzaVidaProject/settings.py
+++ DanzaVidaProject/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['https://dv.villanueva.gob.ar','https://*.127.0.0.1']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-p=_mo$qtv9e%m-x0-2dl!7!k^5m07k0n)#!r)#p!1b0v*hgx!$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'AppFormularios/static'),
+    os.path.join(BASE_DIR, 'AppDanzaVida/static'),
 ]
 
 # Default primary key field type
